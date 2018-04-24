414bb5c359d0bccf0b8ef433c6c369e5d427f239
suarezlui <suarezluis@gmail.com>
2018-04-23 19:57:35 -0500

    finished fight algorithm

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,15 +7,21 @@ var currentEnemy;
 var enemiesDefeated = 0;
 // Building initial ogject caracters
 
-var win = new character("Windows", 100, 7);
-var mac = new character("McOS", 130, 1);
-var ubu = new character("Ubuntu", 110, 5);
-var and = new character("Android", 120, 3);
+var win = new character("Windows", 140, 6);
+var mac = new character("McOS", 100, 10);
+var ubu = new character("Ubuntu", 160, 4);
+var and = new character("Android", 120, 8);
 
 play();
 
 // Defining function to run game
 function play() {
+  if (enemiesDefeated >= 3) {
+    displayInstructions(
+      "Congratulations " + currentPlayer.name + ", you are the greatest OS"
+    );
+  }
+
   if (!playerPicked) {
     pickPlayer();
   }
@@ -37,19 +43,42 @@ function fight() {
     $(".playerArea").animate({ margin: "0 0" }, 500);
     $(".enemyArea").animate({ margin: "0 0" }, 500);
     currentPlayer.health -= currentEnemy.attack;
-    currentEnemy.health -=  currentPlayer.attack;
-    currentPlayer.attack *= 2;
-    $(".playerDamage").animate({ width: (currentPlayer.damagePercentage() + "%" ) }, 800, function() {});
-    $(".enemyDamage").animate({ width: (currentEnemy.damagePercentage()+ "%" ) }, 800, function() {});
-    if(currentPlayer.health <= 0) {
-        $(".btn").off("click"),
+    currentEnemy.health -= currentPlayer.attack;
+    currentPlayer.attack += currentPlayer.initialAttack;
+    $(".playerDamage").animate(
+      { width: currentPlayer.damagePercentage() + "%" },
+      800,
+      function() {}
+    );
+    $(".enemyDamage").animate(
+      { width: currentEnemy.damagePercentage() + "%" },
+      800,
+      function() {}
+    );
+    if (currentPlayer.health <= 0) {
+      $(".btn").off("click"),
         $(".btn").css({ visibility: "visible", opacity: "0" });
-        displayInstructions("You lost to " + currentEnemy.name + " you fight like a Mac")
-    };
-    if(currentEnemy.health <= 0) {
-        $(".btn").off("click");
-        $(".btn").css({ visibility: "visible", opacity: "0" });
-        displayInstructions("You defeated " + currentEnemy.name + " pick a new oponent.")
+      displayInstructions(
+        "You lost to " + currentEnemy.name + " you fight like a Mac"
+      );
+    }
+    if (currentEnemy.health <= 0) {
+      $(".btn").off("click");
+      $(".btn").css({ visibility: "visible", opacity: "0" });
+      displayInstructions(
+        "You defeated " + currentEnemy.name + " pick a new oponent."
+      );
+      $(".enemyImage").css({ visibility: "visible", opacity: "0" });
+      enemyPicked = false;
+      enemiesDefeated += 1;
+      if (enemiesDefeated >= 3) {
+        displayInstructions(
+          "Congratulations " + currentPlayer.name + ", you are the greatest OS"
+        );
+        enemyPicked = true;
+        return;
+      }
+      play();
     }
   });
 }
@@ -60,8 +89,17 @@ function character(name, health, attack) {
   this.health = health;
   this.attack = attack;
   this.initialHealth = health;
-  this.healthPercentage = function() { return 100 * (this.health/this.initialHealth);};
-  this.damagePercentage = function() { if(this.health >= 0) {return 100 - this.healthPercentage();}else{return 100}};
+  this.initialAttack = attack;
+  this.healthPercentage = function() {
+    return 100 * (this.health / this.initialHealth);
+  };
+  this.damagePercentage = function() {
+    if (this.health >= 0) {
+      return 100 - this.healthPercentage();
+    } else {
+      return 100;
+    }
+  };
 }
 
 // Funcion that changes the instruction message
@@ -133,8 +171,20 @@ function pickPlayer() {
 //function to pick next enemy's character
 function pickEnemy() {
   displayInstructions("Pick your enemy OS...");
+  if (enemiesDefeated > 0) {
+    displayInstructions(
+      "You defeated " + currentEnemy.name + ", pick your next enemy OS..."
+    );
+  }
+
   $(".enemy").on("click", function() {
+    $(".enemyDamage").animate({ width: "0%" }, 800, function() {});
+
     if ($(this).attr("value") === "win") {
+      $(".win").animate({ opacity: "0" }, 800, function() {
+        $(".win").css("display", "none");
+      });
+
       $(".enemyCharacter").attr("src", "assets/images/win.gif");
       $(".enemyImage").css({ visibility: "visible", opacity: "1" });
       $(".enemy").off("click");
@@ -145,6 +195,9 @@ function pickEnemy() {
     if ($(this).attr("value") === "mac") {
       $(".enemyCharacter").attr("src", "assets/images/mac.gif");
       $(".enemyImage").css({ visibility: "visible", opacity: "1" });
+      $(".mac").animate({ opacity: "0" }, 800, function() {
+        $(".mac").css("display", "none");
+      });
       $(".enemy").off("click");
       currentEnemy = mac;
       enemyPicked = true;
@@ -153,6 +206,9 @@ function pickEnemy() {
     if ($(this).attr("value") === "ubu") {
       $(".enemyCharacter").attr("src", "assets/images/ubu.gif");
       $(".enemyImage").css({ visibility: "visible", opacity: "1" });
+      $(".ubu").animate({ opacity: "0" }, 800, function() {
+        $(".ubu").css("display", "none");
+      });
       $(".enemy").off("click");
       currentEnemy = ubu;
       enemyPicked = true;
@@ -161,6 +217,9 @@ function pickEnemy() {
     if ($(this).attr("value") === "and") {
       $(".enemyCharacter").attr("src", "assets/images/and.gif");
       $(".enemyImage").css({ visibility: "visible", opacity: "1" });
+      $(".and").animate({ opacity: "0" }, 800, function() {
+        $(".and").css("display", "none");
+      });
       $(".enemy").off("click");
       currentEnemy = and;
       enemyPicked = true;
